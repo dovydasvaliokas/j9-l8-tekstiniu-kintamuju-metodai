a1e8b3e86c8ef1b1368f713a8710cd4bfd3a10d5
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-10-13 10:26:54 +0300

    Padaryti indeksu bei formatavimo pavyzdziai

--- tekstiniu-kintamuju-indeksai.js
+++ tekstiniu-kintamuju-indeksai.js
@@ -14,3 +14,54 @@ console.log(istrauktasTekstas);
 let paskutinesPenkiosRaides = tekstas.slice(-5);            // paskutinės penkios raidės
 console.log(paskutinesPenkiosRaides);
 
+let penkiosRaidesNuoSestos = tekstas.substr(5, 6);
+console.log(penkiosRaidesNuoSestos);
+
+
+console.log(tekstas[2]);
+console.log(tekstas[tekstas.length - 2]);
+console.log(arLygusZodziaiIgnoruojantDydzius(tekstas[2], tekstas[tekstas.length - 2]));     // tekstas[tekstas.length - 2] bus antra nuo galo raidė
+
+
+
+// Paieška tekste KELINTOJE VIETOJE
+let kelintasTekste = tekstas.indexOf("YRa");
+console.log(kelintasTekste);
+
+if (kelintasTekste >= 0) {
+    console.log("Teksto gabalą rado kintamajame");
+}
+else {
+    console.log("Nerado teksto gabalo kintamajame");
+}
+
+
+// Paieška AR YRA TEKSTE
+let arYraTekste = tekstas.includes("tekstas");
+console.log("Ar yra tekste: " + arYraTekste);       // išves false, nes kintamajame yra TEKSTAS, o čia ieškome tekstas
+
+// Paieška AR YRA TEKSTE, ignoruojant raidžių dydžius
+let arYraTekste2 = tekstas.toLocaleLowerCase().includes("tekstas");
+console.log("Ar yra tekste 2: " + arYraTekste2);
+
+
+// teksto užpildymas iš abiejų pusių
+tekstas = tekstas.padStart(tekstas.length + 5, "*");
+console.log(tekstas);
+
+tekstas = tekstas.padEnd(tekstas.length + 3, "#");
+console.log(tekstas);
+
+
+
+
+
+function arLygusZodziaiIgnoruojantDydzius(zodis1, zodis2) {
+    // return zodis1.toLocaleLowerCase() == zodis2.toLocaleLowerCase()
+     if (zodis1.toLocaleUpperCase() == zodis2.toLocaleUpperCase()) {
+         return true;
+     }
+     else {
+         return false;
+     }
+ }
---
+++ teksto-formatavimas.js
@@ -0,0 +1,11 @@
+let x = +prompt("Įveskite pirmą skaičių");
+let y = +prompt("Įveskite antrą skaičių");
+
+let suma = x + y;
+
+console.log("Skaičių " + x + " ir " + y + " suma yra: " + suma);
+
+let suformatuotasTekstas = `Skaičių ${x} ir ${y} suma yra: ${suma}`;
+console.log(suformatuotasTekstas);
+
+console.log(`Skaičių ${x} ir ${y} suma yra: ${suma}`);
